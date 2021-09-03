0dcd80d86ac8ef88a7dea3250b9f055f077e32e4
adelincarabas <63587456+adelincarabas@users.noreply.github.com>
2021-09-03 11:02:33 +0300

    Time: 88 ms (20.13%), Space: 40.3 MB (55.03%) - LeetHub

---
+++ find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var sumZero = function(n) {
+    let result = [];
+  if(n % 2 !== 0) {
+    result.push(0);
+  }
+
+  for(let i = 0; i < Math.floor(n/2); i++) {
+    result.push(n-i,-n+i)
+  }
+    return result;
+};
