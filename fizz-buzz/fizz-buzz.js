ceaf6f78f226bd6aea92325ba0139cc52ebe5577
adelincarabas <63587456+adelincarabas@users.noreply.github.com>
2021-09-07 16:07:26 +0300

    Time: 84 ms (44.01%), Space: 41.2 MB (34.51%) - LeetHub

---
+++ fizz-buzz/fizz-buzz.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+  let b = [];
+    for(i = 1; i <=n; i++) {
+      if(i % 3 == 0 && i % 5 == 0){
+        b.push("FizzBuzz");
+      }else if(i % 3 == 0){
+        b.push("Fizz");
+      }else if(i % 5 == 0){
+        b.push("Buzz");
+      }else{
+      b.push(i.toString());
+      }
+    }
+    return b;
+};
