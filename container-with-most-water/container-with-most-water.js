3c61e520a9f4e19c45d67d3d408487c5e6b74dd3
adelincarabas <63587456+adelincarabas@users.noreply.github.com>
2021-09-08 23:39:24 +0300

    Time: 88 ms (70.27%), Space: 48.5 MB (6.60%) - LeetHub

---
+++ container-with-most-water/container-with-most-water.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+  let p1 = 0;
+  let found = 0;
+  let p2 = height.length-1;
+  while(p1 < p2) {
+     const maxValue = Math.min(height[p1],height[p2]) * (p2-p1);
+     found = Math.max(found,maxValue)
+  if(Math.min(height[p1],height[p2]) == height[p1]){
+    p1++;
+  }else {
+    p2--;
+  }
+  }
+  return found;
+};
